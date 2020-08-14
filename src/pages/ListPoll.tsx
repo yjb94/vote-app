dd541f1f8c09196f57f68890bd556cad9efcf107
yjb94 <jbyun94@naver.com>
2020-08-14 19:27:22 +0900

    Route added for create

--- src/pages/ListPoll.tsx
+++ src/pages/ListPoll.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
 
 const ListPoll: React.FC = () => {
   return (
     <Container>
       list poll
+      <CreatePoll
+        to="/create"
+      >
+        +
+      </CreatePoll>
     </Container>
   )
 };
 
 const Container = styled.div`
+`;
+const CreatePoll = styled(Link)`
 `;
 
 export default ListPoll;
